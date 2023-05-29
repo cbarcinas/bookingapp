a6c2475e0424d4c00de067952dca82cf2621e941
Craig Barcinas <cbarcinas@live.com>
2023-05-29 14:59:36 -0700

    init

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -2,4 +2,13 @@ import express from 'express';
 
 const router = express.Router();
 
+// Create
+router.post("/", (req,res) => {
+    
+})
+// Update
+// Delete
+// Get
+// Get All
+
 export default router;
