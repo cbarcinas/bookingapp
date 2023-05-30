5a7956624001b17a6e86b83edcdc77c4912eb210
Craig Barcinas <cbarcinas@live.com>
2023-05-29 19:08:34 -0700

    created get route: mvp

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -43,6 +43,14 @@ router.delete('/:id', async (req, res) => {
 });
 
 // Get
+router.get('/:id', async (req, res) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    res.status(200).json(hotel);
+  } catch (error) {
+    res.status(500).json(err);
+  }
+});
 // Get All
 
 export default router;
