91f05beabb23e55f4e8892031ecba12a45269b6e
Craig Barcinas <cbarcinas@live.com>
2023-05-29 19:01:39 -0700

    added updated route: mvp

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,12 +1,37 @@
 import express from 'express';
+import Hotel from '../models/Hotel.js';
 
 const router = express.Router();
 
 // Create
-router.post("/", (req,res) => {
-    
-})
+router.post('/', async (req, res) => {
+  const newHotel = new Hotel(req.body);
+
+  try {
+    const savedHotel = await newHotel.save();
+    res.status(200).json(savedHotel);
+  } catch (error) {
+    res.status(500).json(err);
+  }
+});
+
 // Update
+router.put('/:id', async (req, res) => {
+  // id should already exist in db since we are updating existing entry
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedHotel);
+  } catch (error) {
+    res.status(500).json(err);
+  }
+});
+
 // Delete
 // Get
 // Get All
