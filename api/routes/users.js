45e3effc6d4568316b77a5a80bd4fc30ff4e532c
Craig Barcinas <cbarcinas@live.com>
2023-06-02 18:54:10 -0700

    created router for user, wip auth check

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,30 @@
 import express from 'express';
+import {
+  deleteUser,
+  getAllUsers,
+  getUser,
+  updateUser,
+} from '../controllers/userController.js';
+import { verifyToken } from '../utils/verifyToken.js';
 
 const router = express.Router();
+
+// This endpoint will go to verifyToken, if all checks inside
+// verifyToken pass, then it'll hit next() and proceed with endpoint
+router.get('/checkauthentication', verifyToken, (req, res, next) => {
+  res.send('Sucessfully logged in user');
+});
+
+// Update
+router.put('/:id', updateUser);
+
+// Delete
+router.delete('/:id', deleteUser);
+
+// Get
+router.get('/:id', getUser);
+
+// Get All
+router.get('/', getAllUsers);
 
 export default router;
