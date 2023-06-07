ff5f36f7c140a95dd589f946388e0111db91bf5f
Craig Barcinas <cbarcinas@live.com>
2023-06-07 12:23:29 -0700

    verify token, mvp

--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -14,13 +14,3 @@ export const verifyToken = (req, res, next) => {
   });
 };
 
-export const verifyUser = (err, req, res, next) => {
-  verifyToken(req, res, () => {
-    // compare the id from our token to id coming in from path params /checkuser/:id
-    if (req.user.id === req.params.id || req.user.isAdmin) {
-      next();
-    } else {
-      return next(createError(403, 'Access denied, not authorized'));
-    }
-  });
-};
