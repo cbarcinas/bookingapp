4e8dd22d801814d99a34c5264ef42495bb75df1e
Craig Barcinas <cbarcinas@live.com>
2023-06-07 13:06:36 -0700

    validated checks

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,19 +5,9 @@ import {
   getUser,
   updateUser,
 } from '../controllers/userController.js';
-import { verifyToken, verifyUser } from '../utils/verifyToken.js';
+import { verifyAdmin, verifyToken, verifyUser } from '../utils/verifyToken.js';
 
 const router = express.Router();
-
-// When the user reaches /checkauthentication endpoint, it will go redirect to verifyToken, if all checks
-// inside verifyToken pass, then it'll hit next() and proceed with endpoint which is the response
-router.get('/checkauthentication', verifyToken, (req, res, next) => {
-  res.send('hello user, you are logged in');
-});
-
-router.get('/checkuser/:id', verifyUser, (req, res, next) => {
-  res.send('hello, you are logged in and are able to delete account');
-});
 
 // Update
 router.put('/:id', updateUser);
@@ -32,3 +22,19 @@ router.get('/:id', getUser);
 router.get('/', getAllUsers);
 
 export default router;
+
+// Test auth status, verify user or admin status
+
+// When the user reaches /checkauthentication endpoint, it will go redirect to verifyToken, if all checks
+// inside verifyToken pass, then it'll hit next() and proceed with endpoint which is the response
+// router.get('/checkauthentication', verifyToken, (req, res, next) => {
+//   res.send('hello user, you are logged in');
+// });
+
+// router.get('/checkuser/:id', verifyUser, (req, res, next) => {
+//   res.send('hello, you are logged in and are able to delete account');
+// });
+
+// router.get('/checkadmin/:id', verifyAdmin, (req, res, next) => {
+//   res.send('hello admin, you are logged in and are able to delete any account');
+// });
