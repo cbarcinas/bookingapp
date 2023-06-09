faba6aaae8d385973ac7603be09859b12ba81805
Craig Barcinas <cbarcinas@live.com>
2023-06-09 15:26:39 -0700

    imported template fn from hotel controller

--- api/controllers/roomController.js
+++ api/controllers/roomController.js
@@ -24,3 +24,50 @@ export const createRoom = async (req, res, next) => {
 };
 
 
+// Update
+export const updateHotel = async (req, res, next) => {
+  // id should already exist in db since we are updating existing entry
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      {
+        // MongoDB $set
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedHotel);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Delete
+export const deleteHotel = async (req, res, next) => {
+  try {
+    await Hotel.findByIdAndDelete(req.params.id);
+    res.status(200).json('Hotel deleted from database');
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Get
+export const getHotel = async (req, res, next) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    res.status(200).json(hotel);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Get All
+export const getAllHotels = async (req, res, next) => {
+  try {
+    const hotels = await Hotel.find();
+    res.status(200).json(hotels);
+  } catch (err) {
+    next(err);
+  }
+};
