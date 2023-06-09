134e647ccb984d5a35318fc101af9db4e712b6f4
Craig Barcinas <cbarcinas@live.com>
2023-06-09 15:30:18 -0700

    created room routes

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,5 +1,28 @@
 import express from 'express';
+import {
+  createRoom,
+  deleteRoom,
+  getAllRooms,
+  getRoom,
+  updateRoom,
+} from '../controllers/hotelController.js';
+import { verifyAdmin } from '../utils/verifyToken.js';
 
 const router = express.Router();
+
+// Create
+router.post('/', verifyAdmin, createRoom);
+
+// Update
+router.put('/:id', verifyAdmin, updateRoom);
+
+// Delete
+router.delete('/:id', verifyAdmin, deleteRoom);
+
+// Get
+router.get('/:id', getRoom);
+
+// Get All
+router.get('/', getAllRooms);
 
 export default router;
