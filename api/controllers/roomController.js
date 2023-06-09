e82940950806b0b6a131709ee328a5ccf1432f90
Craig Barcinas <cbarcinas@live.com>
2023-06-09 15:26:13 -0700

    completed createRoom fn

--- api/controllers/roomController.js
+++ api/controllers/roomController.js
@@ -4,16 +4,23 @@ import { createError } from '../utils/error.js';
 
 export const createRoom = async (req, res, next) => {
   const hotelId = req.params.hotelid;
+  // Creates new Room using the req body
   const newRoom = new Room(req.body);
 
   try {
+    // Once the user submits the new room we take it from the req body and save to db
     const savedRoom = await newRoom.save();
     try {
-        await Hotel.findByIdAndUpdate(hotelId,{})
+      await Hotel.findByIdAndUpdate(hotelId, {
+        $push: { rooms: savedRoom._id },
+      });
     } catch (err) {
       next(err);
     }
+    res.status(200).json(savedRoom);
   } catch (err) {
     next(err);
   }
 };
+
+
