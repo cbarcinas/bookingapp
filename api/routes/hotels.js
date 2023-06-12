8a2889bcc4d8f3baae922c0a2071218f3a22e8d6
Craig Barcinas <cbarcinas@live.com>
2023-06-11 18:11:56 -0700

    added getByType route

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import {
   countByCity,
+  countByType,
   createHotel,
   deleteHotel,
   getAllHotels,
@@ -26,6 +27,6 @@ router.get('/find/:id', getHotel);
 // Get All
 router.get('/', getAllHotels);
 router.get('/countByCity', countByCity);
-router.get('/countByType', getAllHotels);
+router.get('/countByType', countByType);
 
 export default router;
