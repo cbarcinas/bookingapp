e1382ff06108c5ca008c0361a581781e4e1b65a8
Craig Barcinas <cbarcinas@live.com>
2023-06-16 23:10:25 -0700

    carousel display wip

--- frontend/src/components/featured/Featured.jsx
+++ frontend/src/components/featured/Featured.jsx
@@ -1,4 +1,6 @@
+import { useRef, useEffect, useState } from 'react';
 import useFetch from '../../hooks/useFetch';
+import { motion } from 'framer-motion';
 import './featured.css';
 
 const Featured = () => {
@@ -6,51 +8,64 @@ const Featured = () => {
     '/hotels/countByCity?cities=berlin,madrid,london'
   );
 
-  console.log(data);
+  // width/setWidth will be used to find the
+  // end of the constraints for our carousel
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
+
+  useEffect(() => {
+    console.log(carousel.current);
+  },[]);
 
   return (
-    <div className="featured">
-      {loading ? (
-        'Loading please wait'
-      ) : (
-        <>
-          <div className="featuredItem">
-            <img
-              src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
-              alt=""
-              className="featuredImg"
-            />
-            <div className="featuredTitles">
-              <h1>Berlin</h1>
-              <h2>{data[0]}</h2>
-            </div>
-          </div>
+    <motion.div ref={carousel} className="featuredCarousel">
+      <motion.div
+        drag="x"
+        dragConstraints={{ right: 0 }}
+        className="featuredInnerCarousel"
+      >
+        {loading ? (
+          'Loading please wait'
+        ) : (
+          <>
+            <motion.div className="featuredItem">
+              <img
+                src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+                alt=""
+                className="featuredImg"
+              />
+              <div className="featuredTitles">
+                <h1>Berlin</h1>
+                <h2>{data[0]}</h2>
+              </div>
+            </motion.div>
 
-          <div className="featuredItem">
-            <img
-              src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o="
-              alt=""
-              className="featuredImg"
-            />
-            <div className="featuredTitles">
-              <h1>Madrid</h1>
-              <h2>{data[1]}</h2>
-            </div>
-          </div>
-          <div className="featuredItem">
-            <img
-              src="https://cf.bstatic.com/xdata/images/city/max500/689422.webp?k=2595c93e7e067b9ba95f90713f80ba6e5fa88a66e6e55600bd27a5128808fdf2&o="
-              alt=""
-              className="featuredImg"
-            />
-            <div className="featuredTitles">
-              <h1>London</h1>
-              <h2>{data[2]}</h2>
-            </div>
-          </div>
-        </>
-      )}
-    </div>
+            <motion.div className="featuredItem">
+              <img
+                src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o="
+                alt=""
+                className="featuredImg"
+              />
+              <div className="featuredTitles">
+                <h1>Madrid</h1>
+                <h2>{data[1]}</h2>
+              </div>
+            </motion.div>
+            <motion.div className="featuredItem">
+              <img
+                src="https://cf.bstatic.com/xdata/images/city/max500/689422.webp?k=2595c93e7e067b9ba95f90713f80ba6e5fa88a66e6e55600bd27a5128808fdf2&o="
+                alt=""
+                className="featuredImg"
+              />
+              <div className="featuredTitles">
+                <h1>London</h1>
+                <h2>{data[2]}</h2>
+              </div>
+            </motion.div>
+          </>
+        )}
+      </motion.div>
+    </motion.div>
   );
 };
 
