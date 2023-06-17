870c5693ad2cb7e54d4ccaa51de6243e78c1c845
Craig Barcinas <cbarcinas@live.com>
2023-06-17 12:59:09 -0700

    imported carousel and added width state & others

--- frontend/src/components/propertyList/PropertyList.jsx
+++ frontend/src/components/propertyList/PropertyList.jsx
@@ -1,8 +1,16 @@
+import { useRef, useEffect, useState } from 'react';
 import useFetch from '../../hooks/useFetch';
+import { motion } from 'framer-motion';
+
 import './propertyList.css';
 
 const PropertyList = () => {
   const { data, loading, error } = useFetch('/hotels/countByType');
+
+  // width/setWidth will be used to find the
+  // end of the constraints for our carousel
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
 
   const images = [
     'https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o=',
@@ -13,28 +21,33 @@ const PropertyList = () => {
   ];
 
   return (
-    <div className="pList">
-      {loading ? (
-        "loading"
-      ) : (
-        <>
-          {data &&
-            images.map((img,i) => (
-              <div className="pListItem" key={i}>
-                <img
-                  src={img}
-                  alt=""
-                  className="pListImg"
-                />
-                <div className="pListTitles">
-                  <h1>{data[i]?.type}</h1>
-                  <h2>{data[i]?.count} {data[i]?.type}</h2>
-                </div>
-              </div>
-            ))}
-        </>
-      )}
-    </div>
+    <>
+      <motion.div className="pListCarousel">
+        <motion.div
+          drag="x"
+          dragConstraints={{ right: 0 }}
+          className="pListInnerCarousel"
+        >
+          {loading ? (
+            'loading'
+          ) : (
+            <>
+              {data &&
+                images.map((img, i) => (
+                  <div className="pListItem" key={i}>
+                    <img src={img} alt="" className="pListImg" />
+                    <div className="pListTitles">
+                      <h2>
+                        {data[i]?.count} {data[i]?.type}
+                      </h2>
+                    </div>
+                  </div>
+                ))}
+            </>
+          )}
+        </motion.div>
+      </motion.div>
+    </>
   );
 };
 
